fb60da61fe496bb897fe57022480bf0f8f930011
Daniel Ng <ducduy.ababat@gmail.com>
2019-06-30 17:19:54 +0700

    remove app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -24,6 +23,6 @@ import { ConfigModule } from './config/config.module';
     ConfigModule,
   ],
   controllers: [AppController, UserController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
